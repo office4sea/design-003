81dfe24362986421d94dfbb167e4375da60c98d1
hskang <parts1123@gmail.com>
2023-01-03 22:15:32 +0900

    khs/라이브러리 작업

--- dist/bridge-emulator.js
+++ dist/bridge-emulator.js
@@ -1,36 +1,56 @@
-bada.binder('navigator', binder=> {
-    const logger = bada.getLogger(binder.name);
+// 버튼 그룹
+bada.binder('nav-bar', binder=> {
+    const logger = bada.logger(binder.name);
 
+    // 디버깅 모드 전환
     const {debugMode} = binder.vo;
-    debugMode.text = bada.useLogger ? '로깅 비활성화' : '로깅 활성화';
+    debugMode.text = bada.logger.level ? '디버깅 비활성화' : '디버깅 활성화';
     debugMode.event('click', _=> {
-        bada.useLogger = bada.useLogger ? 0 : 1;
+        bada.logger.level = bada.logger.level ? '' : 'debug';
         location.reload();
     });
 
+    // 브릿지 네이티브
     const {bridgeNative} = binder.vo;
     bridgeNative.event('click', _=> {
-        const {bridgeNative} = bada.binder;
-        bridgeNative.show();
+        bada.binder.bridgeNative.show();
     });
 
+    // 역브릿지(네이티브 -> 웹)
     const {bridgeWeb} = binder.vo;
     bridgeWeb.event('click', _=> {
-        const {bridgeNative} = bada.binder;
-        bridgeNative.hide();
     });
 });
 
+// 브릿지 네이티브
 bada.binder('bridge-native', binder=> {
-    const logger = bada.getLogger(binder.name);
+    const logger = bada.logger(binder.name);
+
+    // 닫기
+    const {vo:{close}} = binder;
+    close.event('click', _=> binder.hide());
+
+    // 브릿지 목록
+    const {vo:{commnads, receiveData}} = binder;
+    const stubBridge = bada.bridge.getStub();
+    commnads.event('change', _=> {
+        if(!commnads.value) return;
+
+        receiveData.value = JSON.stringify(stubBridge.readMessage(commnads.value), '', '  ');
+    });
 
     /**@override */
-    binder.show = _=> {
-        logger.info('-show-');
-        binder.el.style.display = 'block';
+    binder.show=_=> {
+        // ui 노출
+        binder.addClass('active');
+        logger.debug('--브릿지 건수--', commnads.children.length - 1);
+        if(commnads.children.length > 1) return;
+
+        stubBridge.getMessages()
+        .forEach(value=> commnads.appendChild(
+            binder.getTemplate('option-item', item=> Object.assign(item, {value, text: value}))
+        ));
     };
     /**@override */
-    binder.hide = _=> {
-        binder.el.style.display = 'none';
-    };
+    binder.hide=_=>binder.removeClass('active');
 });
--- libs/bada.d.js
+++ libs/bada.d.js
@@ -1,96 +1,129 @@
 // ===== 기본 타입 =====
 /**
- * @typedef {Object<string, string>} StringDataObject
+ * @typedef {object} PropertyAttribute
+ * @property {()=> any} PropertyAttribute.get
+ * @property {(vl: any)=> void} PropertyAttribute.set
  */
 
-// ===== 웹 어플리 케이션 베이스 =====
+// ===== App =====
 /**
- * @typedef {object} App 어플리 케이션
- * @property {Utils} App.utils 유틸리티 객체
- * @property {WebStorage} App.storage 브라우저 스토리지 관리
- * @property {Binder} App.binder DOM 엘리먼트 바인더
- * @property {{(name: string)=> Bridge} & Bridge} App.bridge 네이티브 브릿지
- * @property {(label: string, style?: string)=> Logger} App.getLogger 로깅 객체 반환
- * @property {0|1|2|3} App.useLogger 로그 여부 설정
- * * 0: 비활성화
- * * 1: 시스템레벨
- * * 2: 사용자레벨
- * * 3: 경고
- * @property {()=> boolean} App.hasDepend 의존성 객체 폼함 여부
- * @property {(name:string, attribute: {get():any, set(v:any):void})=> App} App.property 프로퍼티 추가
+ * @typedef {object} App App
+ * @property {Utils} App.utils 유틸리티
+ * @property {Bridge} App.bridge 네이티브 브릿지
+ * @property {{session: WebStorage, local: WebStorage}} App.storage 스토리지
+ * @property {{(label: string, style?: string)=> Logger, level: LoggerLevels}} App.logger 로그 출력용 객체 반환
+ * @property {{(id: string, onBind?: (binder: Binder)=> void) => Binder, [k: string]: Binder}} App.binder 엘리먼트 바인더
  */
 
-// ===== 유틸리티 =====
+// ===== Utils =====
 /**
- * @typedef {object} Utils 유틸리티
- * @property {(target: any, name: string, attribute: {get?(): any, set?(v: any): void})=> any} Utils.prop 객체 프로퍼티 추가
- * @property {(v: string='abcd')=> string} Utils.camelCase 카멜케이스
+ * @typedef {'yyyy-mm-dd' | 'hh:mi:ss' | 'yyyy-mm-dd hh:mi:ss.ms'} DateFormat
+ * 
+ * @typedef {object} Utils
+ * @property {(v: string)=> string} Utils.camelCase 카멜케이스
  * @property {(v: string)=> string} Utils.pascalCase 파스칼케이스
  * @property {(v: string)=> string} Utils.snakeCase 스네이크케이스
  * @property {(v: string)=> string} Utils.kebabCase 케밥케이스
- * @property {(format?: 'yyyy/mm/dd hh:mi:ss.ms', date=new Date)=> string} Utils.formatDate 날짜에 대한 포멧 형식 반환
+ * @property {(target: any, name: string, attr: ()=> void | PropertyAttribute)=> any} Utils.seal 변경 불가 속성 추가
+ * @property {(format?: DateFormat, date=new Date)=> string} Utils.formatDate 날짜에 대한 포멧 형식 반환
  */
 
-// ===== 웹스토리지 =====
+// ===== 스토리지 =====
 /**
- * @typedef {{local: WebStorageCollection, session: WebStorageCollection, cookie: WebStorageCollection}} WebStorage 웹스토리지
+ * @typedef {StorageMain & {(...keys: string): StorageMain, [k: string]: StorageGroup}} WebStorage
  * 
- * @typedef {WebStorageInterface & Object<string, WebStorageItem>} WebStorageCollection 웹스토리지 콜렉션
+ * @typedef {object} StorageMain
+ * @property {()=> void} StorageMain.clear 스토리지 공간 삭제
+ * @property {(...keys: string)=> StorageMain} StorageMain.setGroup 그룹 설정
  * 
- * @typedef {object} WebStorageInterface 웹스토리지 인터페이스
- * @property {(keys: string|Array<string>)=> void} WebStorageInterface.setKeys 키값 등록
- * @property {()=> void} WebStorageInterface.clear 스토리지 전체 공간 삭제
+ * @typedef {StorageItem & {(...keys: string): StorageGroup, clear(): StorageGroup, [k: string]: StorageItem}} StorageGroup
+ * @property {string} StorageGroup.clear 그룹 공간 삭제
  * 
- * @typedef {object} WebStorageItem 웹스토리지 아이템 항목
- * @property {string} WebStorageItem.str 스트링값
- * @property {string} WebStorageItem.json JSON 객체
- * @property {()=> void} WebStorageItem.remove 스토리지 삭제
+ * @typedef {object} StorageItem 스토리지 아이템
+ * @property {string} StorageItem.str 스트링
+ * @property {string} StorageItem.json JSON 객체
+ * @property {()=> void} StorageItem.remove 항목 삭제
  */
 
+// ===== 로깅 =====
+/**
+ * @typedef {'debug'|'info'|'warn'|'error'} LoggerLevels 출력할 로그 레벨 debug> info> warn> error
+ * 
+ * @typedef {object} Logger
+ * @property {void} LoggerItem.trace 스택 브레이크
+ * @property {(...arg: any)=> void} LoggerItem.debug 디버그 로그 출력
+ * @property {(...arg: any)=> void} LoggerItem.info 정보 로그 출력
+ * @property {(...arg: any)=> void} LoggerItem.warn 경고 로그 출력
+ * @property {(...arg: any)=> void} LoggerItem.error 오류 로그 출력
+ */
+
+// ===== 엘리먼트 바인더 =====
+/**
+ * @typedef {object} Binder
+ * @property {string} Binder.name 바인더 이름
+ * @property {HTMLElement} Binder.el 바인딩된 엘리먼트
+ * @property {(name: string, onCreate:(binder: Binder)=> void)=> Binder} Binder.getTemplate 템플릿(data-template) 객체 반환
+ * @property {Object<string, BindVoHandle>} Binder.vo 연결(data-vo)된 컨트롤 엘리먼트 객체
+ * @property {(v: Object<string, string>)=> Binder} Binder.setText 수집된 컨트롤에 TEXT 출력
+ * @property {()=> Object<string, string>} Binder.getText 수집된 컨트롤에 입력된 TEXT 반환
+ * @property {(v: Object<string, string>)=> Binder} Binder.setHtml 수집된 컨트롤에 HTML 마커블 적용
+ * @property {()=> Object<string, string>} Binder.getHtml 수집된 컨트롤에 입력된 HTML 반환
+ * @property {(v: Object<string, string>)=> Binder} Binder.setValue 수집된 컨트롤에 Value 값 출력
+ * @property {()=> Object<string, string>} Binder.getValue 수집된 컨트롤에 입력된 Value 반환
+ * @property {(...name: string)=> Binder} Binder.addClass 클래스 추가
+ * @property {(...name: string)=> Binder} Binder.removeClass 클래스 삭제
+ * @property {(name: string)=> boolean} Binder.hasClass 클래스 존재 여부
+ * @property {()=> void} Binder.show 엘리먼트 노출
+ * @property {()=> void} Binder.hide 엘리먼트 숨김
+ * 
+ * @typedef {HTMLElement & VoHandle} BindVoHandle 화면 컨트롤러
+ * 
+ * @typedef {object} VoHandle 화면 컨트롤러
+ * @property {string} VoHandle.text TEXT 입/출력
+ * @property {string} VoHandle.html HTML 마커블 입/출력
+ * @property {()=> VoHandle} VoHandle.empty 내부 노드 비우기
+ * @property {(type: keyof HTMLElementEventMap, listener: (event: Event)=> void)=> BindVoHandle} VoHandle.event 이벤트 등록
+ * @property {(...name: string)=> VoHandle} VoHandle.addClass 클래스 추가
+ * @property {(...name: string)=> VoHandle} VoHandle.removeClass 클래스 삭제
+ * @property {(name: string)=> boolean} VoHandle.hasClass 클래스 존재 여부
+ */
 
 // ===== 네이티브 브릿지 =====
 /**
- * @typedef {object} Bridge 네이티브 브릿지
- * @property {(command: string, payload: any)=> Promise<any>} Bridge.postMessage 네이티브에 수행 명령에 대한 메시지 요청
- * @property {(command: string, struct: {payload: any, receive: any})=> Bridge} Bridge.addCommand 네이티브에서 수행 할 명령어 등록
- * @property {(command: string, listener: (receive: any)=> void)=> Bridge} Bridge.addEventListener 네이티브로 부터 호출 될 이벤트 리스너 등록
+ * @typedef {BridgeInterface & {[k: string]: (param: any)=> Promise<any>}} Bridge 네이티브 브릿지
  * 
- * @typedef {{header: BridgeMessageHeader, body: any}} BridgePostMessage 브릿지 요청 메시지
+ * @typedef {object} BridgeInterface 브릿지 인터페이스
+ * @property {()=> BridgeStub} BridgeInterface.getStub 브릿지 스터빙 객체
+ * @property {(command: string, param?: object)=> Promise<any>} BridgeInterface.postMessage 브릿지 메시지 전달
+ * @property {(command: string, payload?: object, receive?: object)=> Bridge} BridgeInterface.setMessage 브릿지 메시지 등록
+ * @property {(command: string, listener: (param: any)=> void)=> Bridge} BridgeInterface.addEventListener 브릿지 이벤트 리스너 등록
  * 
- * @typedef {{header: BridgeMessageHeader, body: {result?: any, error?: any}}} BridgeReceiveMessage 브릿지 요청 결과 메시지
+ * @typedef {object} BridgeStub 브릿지 스텁
+ * @property {()=> void} BridgeStub.clearMessages 메시지 커맨드 비우기
+ * @property {()=> Array<string>} BridgeStub.getMessages 등록된 메시지 커맨드
+ * @property {(command: string)=> BridgeMessageBody} BridgeStub.readMessage 메시지 결과 더미값 반환
+ * @property {(command: string, result?: object, error?:object)=> void} BridgeStub.writeMessage 메시지 결과 더미값 반환
  * 
- * @typedef {object} BridgeMessageHeader 브릿지 요청 메시지 헤더
- * @property {string} BridgeMessageHeader.trid 트렌젝션 아이디
- * @property {string} BridgeMessageHeader.command 수행 명령
+ * @typedef {object} BridgeMessageBody 브릿지 메시지 바디
+ * @property {object=} BridgeMessageBody.error 오류
+ * @property {object=} BridgeMessageBody.payload 메시지 요청 데이터
+ * @property {object=} BridgeMessageBody.result 메시지 응답 데이터
  */
 
-// ===== 화면 엘리먼트 연결 =====
-/**
- * @typedef {{(id:string, onBind?: (binder: BindHandle)=> void)=> void, [x: string]: BindHandle}} Binder 화면 엘리먼트 연결
- * 
- * @typedef {object} BindHandle 바인딩 핸들러
- * @property {string} BinderHandle.name 핸들러 이름
- * @property {HTMLElement} BinderHandle.el 바인딩된 엘리먼트
- * @property {Object<string, BindVoHandle>} BinderHandle.vo 연결(data-vo)된 컨트롤 엘리먼트 객체
- * @property {(v: StringDataObject)=> BindHandle} BinderHandle.setText 연결된 컨트롤 엘리먼트에 TEXT 출력
- * @property {()=> StringDataObject} BinderHandle.getText 연결된 컨트롤 엘리먼트에 입력된 TEXT 반환
- * 
- * @typedef {object} BindVoHandle 화면 컨트롤러
- * @property {HTMLElement} BindVoHandle.el 바인딩된 엘리먼트
- * @property {string} BindVoHandle.text TEXT 입/출력
- * @property {(type: keyof HTMLElementEventMap, listener: (event: Event)=> void)=> BindVoHandle} BindVoHandle.event 이벤트 등록
- */
-
-// ===== 로깅객체 =====
-/**
- * @typedef {object} Logger 로깅객체
- * @property {(level: 'global'|'info'|'warn')=> void} Logger.useLogger
- * @property {(...v: any)=> void} Logger.global 콘솔 로그(시스템) 출력
- * @property {(...v: any)=> void} Logger.info 콘솔 로그(사용자) 출력
- * @property {(...v: any)=> void} Logger.warn 콘솔 경고 출력(호출 스택 포함)
- * @property {(...v: any)=> void} Logger.error 콘솔 오류 출력(호출 스택 포함)
- * @property {()=> void} Logger.brake 디버그 모드(info 레벨)에서 중단점 설정
- */
+// /**
+//  * @typedef {object} Bridge 네이티브 브릿지
+//  * @property {(command: string, payload: any)=> Promise<any>} Bridge.postMessage 네이티브에 수행 명령에 대한 메시지 요청
+//  * @property {(command: string, struct: {payload: any, receive: any})=> Bridge} Bridge.addCommand 네이티브에서 수행 할 명령어 등록
+//  * @property {(command: string, listener: (receive: any)=> void)=> Bridge} Bridge.addEventListener 네이티브로 부터 호출 될 이벤트 리스너 등록
+//  * 
+//  * @typedef {{header: BridgeMessageHeader, body: any}} BridgePostMessage 브릿지 요청 메시지
+//  * 
+//  * @typedef {{header: BridgeMessageHeader, body: {result?: any, error?: any}}} BridgeReceiveMessage 브릿지 요청 결과 메시지
+//  * 
+//  * @typedef {object} BridgeMessageHeader 브릿지 요청 메시지 헤더
+//  * @property {string} BridgeMessageHeader.trid 트렌젝션 아이디
+//  * @property {string} BridgeMessageHeader.command 수행 명령
+//  */
 
 /**
  * 웹 어플리케이션
