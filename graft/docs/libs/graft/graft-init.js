bf4ad8d215d17e54691958d974f251d9d642455c
hskang <parts1123@gmail.com>
2023-08-24 13:28:55 +0900

    khs/ Graft JS 추가

--- graft/docs/libs/graft/graft-init.js
+++ graft/docs/libs/graft/graft-init.js
@@ -6,23 +6,6 @@ const gf= Graft.getInstance(gf=> {
         isMobile: /android|iphone/i.test(navigator.userAgent),
     };
 
-    // 웹 콘솔 디버거 추가
-    // if(gf.agent.isDev && gf.agent.isMobile) {
-    //     gf.log.addDebugger('/web/libs/_dev/eruda.js')
-    //         .then(_=> {
-    //             gf.agent.isDev && Graft.import('/web/libs/_dev/graft-bridge.mock.js');
-    //         });
-    // } else {
-    //     gf.agent.isDev && Graft.import('/web/libs/_dev/graft-bridge.mock.js');
-    // }
-    gf.log.setDebugger({
-        isAdd: gf.agent.isDev && gf.agent.isMobile,
-        url: '/web/libs/_dev/eruda.js'
-    })
-    .then(_=> gf.agent.isDev && Graft.import('/web/libs/_dev/graft-bridge.mock.js'));
-
-
-    gf.log.active('debug');
     // 네이티브 세팅
     gf.bridge.setNative(_=> {
         const nativeName= 'graftNative';
@@ -38,9 +21,29 @@ const gf= Graft.getInstance(gf=> {
         return gf.bridge.mock;
     });
 
-    gf.log.out('어플리케이션 환경 설정');
-    gf.log.out('에이전트 정보', gf.agent);
-    gf.log.out('웹 콘솔 디버거 추가', gf.agent.isDev && gf.agent.isMobile);
+    gf.log.active('debug');
+    // 웹 콘솔 디버거 추가
+    setDebugger('/web/libs/_dev/eruda.js')
+    .then(({isDev})=> {
+        isDev&& Graft.import('/web/libs/_dev/graft-bridge.mock.js');
+
+        gf.log.out('어플리케이션 환경 설정');
+        gf.log.out('에이전트 정보', gf.agent);
+        gf.log.out('웹 콘솔 디버거 추가', gf.agent.isDev && gf.agent.isMobile);
+    });
+
+
+    // ========== 내부기능 ==========
+    function setDebugger(url) {
+        const {agent}= gf;
+        if(!(agent.isDev && agent.isMobile)) return Promise.resolve(agent); 
+        else {
+            return Graft.import(url).then(_=> {
+                eruda.init(), gf.log.refresh();
+                return agent;
+            });
+        }
+    }
 });
 
 // Graft.getInstance(_=> console.log('----싱글톤...------'));
